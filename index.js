99bd3a45c6127bba03fa76bb2629fb0d7818bc05
peaceful-changelog-0333 <mike@zerobyone.com>
2020-09-15 02:12:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var greeting = "";
+var greeting = "Hello, Everyone"!
+
+
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello, Everyone!" + specialGuest + "!";
 
 var topic = "space";
 
